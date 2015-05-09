b09133ed6a74253cf945637cc59db4c82f7de56d
dieterrosch <dieter.rosch@gmail.com>
2015-05-09 21:02:08 +0200

    Added comment to start.sh

--- sickrage.sh
+++ sickrage.sh
@@ -1,1 +1,2 @@
+#A simple shell script to start my version of sickrage
 sudo docker run -d --restart=always -h black-pearl.local -v /home/pi/configs/sickrage:/config -v /media/media_share:/data -p 8081:8081 dieterrosch/rpi-docker-sickrage
